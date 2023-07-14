55eff71dfa0694e9d3530f90585e4150b937b358
B1UG <noor.feraas@gmail.com>
2023-07-15 00:14:34 +0300

    filed added from my PC

--- 13th July 2023 Tasks/tasks.js
+++ 13th July 2023 Tasks/tasks.js
@@ -1,3 +1,4 @@
+// Task1
 let cardsContainer = document.querySelector(".cards-container");
 
 function createCard(cardTitle, cardAge) {
@@ -31,6 +32,105 @@ let cardAges = ['age1', 'age2', 'age3', 'age4'];
 for(let i = 0; i < 4; i++) {
     createCard(cardTitles[i], cardAges[i]);
 }
+
+/* --------------------------------------------- */
+
+// Task2
+let task2 = document.querySelector(".task2");
+let task2Para = document.querySelector(".task2 p");
+
+task2Para.textContent = "Can i help you?";
+task2Para.style = "background-color: yellow; text-align: center; font-size: 30px; "
+
+task2Para.onmouseover = () => {
+    task2Para.textContent = "Hello World!";
+}
+
+task2Para.onmouseleave = () => {
+    task2Para.textContent = "Can i help you?"
+}
+
+/* --------------------------------------------- */
+
+// Task3
+
+let conutries = document.querySelector(".task3 select");
+let flag = document.querySelector(".task3 img");
+
+if(conutries.value === "jordan") {
+    flag.src = "Jordan.png";
+}
+
+conutries.onchange = () => {
+    if(conutries.value === "jordan") {
+        flag.src = "Jordan.png";
+    }
+    else if(conutries.value === "palestine") {
+        flag.src = "palestine.png";
+    }
+}
+
+/* --------------------------------------------- */
+
+// Task4
+
+let fonts = document.querySelectorAll(".task4 select");
+
+let fontDecorations = document.querySelectorAll(".task4 input");
+
+let task4Para = document.querySelector(".task4 p");
+
+task4Para.style = "border: 3px solid black; padding: 20px; height: 240px; width: 600px"
+
+fonts[0].onchange = () => {
+    task4Para.style.fontFamily = `${fonts[0].value}`;
+}
+
+fonts[1].onchange = () => {
+    task4Para.style.fontSize = `${fonts[1].value}`;
+}
+
+// fontDecorations[0].onchange = () => {
+//     if(fontDecorations[0].checked) {
+//         console.log(fontDecorations[0].checked);
+//     }
+//     else {
+//         console.log(fontDecorations[0].checked);
+//     }
+// }
+
+for(let i = 0; i < fontDecorations.length; i++) {
+    fontDecorations[i].onchange = () => {
+        if(fontDecorations[i].checked) {
+            if(fontDecorations[i].id == "italic") {
+                task4Para.style.fontStyle = `italic`;
+            }
+            else if (fontDecorations[i].id == "bold") {
+                task4Para.style.fontWeight = `bold`;
+            }
+            else {
+                task4Para.style.textDecoration = `underline`;
+            }
+        }
+        else {
+            task4Para.style.fontStyle = "initial";
+            task4Para.style.fontWeight = "initial";
+            task4Para.style.textDecoration = "initial";
+        }
+    } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
