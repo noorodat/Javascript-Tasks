e9d7de38f3f434cc118e35cff736c58f63d8eb54
B1UG <noor.feraas@gmail.com>
2023-07-17 20:36:01 +0300

    added

--- 17th July 2023 Tasks/task.js
+++ 17th July 2023 Tasks/task.js
@@ -1,17 +1,46 @@
 let loadBtn = document.querySelector(".task .load-data");
-
+let tableBody = document.querySelector(".task table tbody");
 let req = new XMLHttpRequest();
 
 req.open('GET', 'task.json');
 
-req.onload = function() {
-    const jsGroup = JSON.parse(this.responseText);
-    for(let i = 0; i < jsGroup.names.length; i++) {
-        let tableRow = document.createElement("tr");
-        let tableDate = document.createElement("td");
+function loadData() {
+    const jsGroup = JSON.parse(req.responseText);
 
+    for(let i = 0; i < jsGroup.names.length; i++) {    
+        let tableRow = document.createElement("tr");
         
-    }   
+        let nameData = document.createElement("td");
+        nameData.textContent = `${jsGroup.names[i]}`;
+        tableRow.appendChild(nameData);
+
+        let ageData = document.createElement("td");
+        ageData.textContent = `${jsGroup.ages[i]}`;
+        tableRow.appendChild(ageData);
+
+        let majorData = document.createElement("td");
+        majorData.textContent = `${jsGroup.majors[i]}`;
+        tableRow.appendChild(majorData);
+
+        let uniData = document.createElement("td");
+        uniData.textContent = `${jsGroup.universities[i]}`;
+        tableRow.appendChild(uniData);
+        
+        let imgData = document.createElement("td");
+        let img = document.createElement("img");
+        img.style.width = "50px"
+        img.src = `${jsGroup.image}`;
+        imgData.appendChild(img);
+        tableRow.appendChild(imgData);
+
+        tableBody.appendChild(tableRow);
+        
+    }
+    loadBtn.removeEventListener('click', loadData);
+    
 }
+
+loadBtn.addEventListener('click', loadData);
+
 
 req.send();
